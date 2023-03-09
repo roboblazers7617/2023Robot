4359230e49052e9fc58a50fdf5f6afb868d81c80
SamohAwesome <72154565+SamohAwesome@users.noreply.github.com>
2023-03-08 20:26:01 -0500

    Simple Auto works

--- src/main/java/frc/robot/commands/ArmStuff/AutonMoveToScore.java
+++ src/main/java/frc/robot/commands/ArmStuff/AutonMoveToScore.java
@@ -6,23 +6,24 @@ package frc.robot.commands.ArmStuff;
 
 import java.util.function.Supplier;
 
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.PieceType;
 import frc.robot.Constants.ScoreLevel;
 import frc.robot.subsystems.Arm;
-import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Wrist;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class SimpleScore extends SequentialCommandGroup {
-  /** Creates a new SimplePickup. */
-  public SimpleScore(Arm arm, Wrist wrist, Intake intake, Supplier<PieceType> piece, Supplier<ScoreLevel> level) {
+public class AutonMoveToScore extends SequentialCommandGroup {
+  /** Creates a new SimpleMoveArmToPosition. */
+  public AutonMoveToScore(Arm arm, Wrist wrist, Supplier<ScoreLevel> level, Supplier<PieceType> piece) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new AutonMoveToScore(arm, wrist, level, piece), new OutakePiece(intake, piece),
-        Commands.waitSeconds(1), new StowAuton(arm, wrist, intake));
+    addCommands(new InstantCommand(() -> arm.setPosition(arm.evalScorePosition(level, piece))), arm.WaitUntilArmInPosition(),
+    arm.actuateSuperstructureCommandScore(level, piece),
+    new InstantCommand(() -> wrist.setPosition(wrist.evalScorePosition(level, piece), arm::getArmAngle), wrist),
+    wrist.WaitUntilWristInPosition());
   }
 }
