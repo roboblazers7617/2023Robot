d3790feb5bfcab0426e3d2d68624b6b7ffd6d8d5
Cephandrius42 <94200651+Cephandrius42@users.noreply.github.com>
2023-04-12 19:29:09 -0400

    Add tilt down wrist command

---
+++ src/main/java/frc/robot/commands/ArmStuff/TiltWristDown.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ArmStuff;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Wrist;
+
+public class TiltWristDown extends SequentialCommandGroup {
+  /** Creates a new TiltWristDown. */
+  public TiltWristDown(Arm arm, Wrist wrist) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addCommands(new InstantCommand(() -> wrist.setPosition(wrist.getWristPosition()-14, () -> arm.getArmAngle())), 
+    wrist.WaitUntilWristInPosition());
+  }
+
+}
