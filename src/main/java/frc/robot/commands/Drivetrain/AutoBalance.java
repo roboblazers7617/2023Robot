42cea01db91d7ce8b7cfcad7d2e1e254e55c72d0
Cephandrius42 <94200651+Cephandrius42@users.noreply.github.com>
2023-04-12 20:08:47 -0400

    Autobalance E-Stop Added
    numbers are guestimites and need to be tuned.

--- src/main/java/frc/robot/commands/Drivetrain/AutoBalance.java
+++ src/main/java/frc/robot/commands/Drivetrain/AutoBalance.java
@@ -8,6 +8,8 @@ import javax.lang.model.util.ElementScanner14;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.Drivetrain;
@@ -18,38 +20,34 @@ public class AutoBalance extends
   Drivetrain mDrivetrain;
   PIDController controller = new PIDController(DrivetrainConstants.KP_BALANCE, DrivetrainConstants.KI_BALANCE,
       DrivetrainConstants.KD_BALANCE);
+  private double deltaAngle;
+  private double deltaDistance;
+  private double angleOffset;
+  private double distanceOffset;
+  private boolean willEnd = false;
 
   public AutoBalance(Drivetrain drivetrain) {
     // Use addRequirements() here to declare subsystem dependencies.
     mDrivetrain = drivetrain;
     addRequirements(drivetrain);
-    // TODO: Lukas. (High) Make this a constant
     controller.setSetpoint(0);
     controller.setTolerance(DrivetrainConstants.BALANCING_TOLERANCE);
     controller.enableContinuousInput(-180, 180);
-
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     mDrivetrain.turnOnBrakes(true);
+    distanceOffset = mDrivetrain.getPose2d().getX();
+    angleOffset = mDrivetrain.getGyroAngle();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  /*   if (mDrivetrain.getPitch() <= -DrivetrainConstants.BALANCE_SPEED_BOOST_TOLERANCE) {
-      mDrivetrain.arcadeDrive(
-          MathUtil.clamp(controller.calculate(mDrivetrain.getPitch() + .2), -DrivetrainConstants.MAX_BALANCE_SPEED,
-              DrivetrainConstants.MAX_BALANCE_SPEED),
-          0);
-    } else if (mDrivetrain.getPitch() >= DrivetrainConstants.BALANCE_SPEED_BOOST_TOLERANCE) {
-      mDrivetrain.arcadeDrive(
-          MathUtil.clamp(controller.calculate(mDrivetrain.getPitch() ), -DrivetrainConstants.MAX_BALANCE_SPEED,
-              DrivetrainConstants.MAX_BALANCE_SPEED),
-    0);
-    }*/
+    deltaAngle = mDrivetrain.getGyroAngle() - angleOffset;
+    deltaDistance = mDrivetrain.getPose2d().getX()- distanceOffset;
      if(Math.abs(mDrivetrain.getPitch()) > DrivetrainConstants.BALANCING_TOLERANCE) {
       mDrivetrain.arcadeDrive(
         MathUtil.clamp(controller.calculate(mDrivetrain.getPitch()), -DrivetrainConstants.MAX_BALANCE_SPEED,
@@ -65,6 +63,8 @@ public class AutoBalance extends
     else {
       mDrivetrain.arcadeDrive(0, 0);
     }
+    if((Math.abs(deltaAngle) < 3) && (Math.abs(deltaDistance) > Units.feetToMeters(2)))
+        willEnd = true;      
   }
 
   // Called once the command ends or is interrupted.
@@ -77,6 +77,6 @@ public class AutoBalance extends
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return willEnd;
   }
 }
